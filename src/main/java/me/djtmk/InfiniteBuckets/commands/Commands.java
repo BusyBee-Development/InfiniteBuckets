5ba0c8b947a128a157094c84b071f3fb78dd156a
djtmk-admin <troymkrause@gmail.com>
2025-04-03 23:36:08 -0400

    Simplify commands and remove economy dependency
    Refactored commands to streamline usage under a single /inf command with subcommands for "water", "lava", "reload", and "give". Removed Vault and economy-based costs, focusing purely on permissions for access control. Enhanced bucket functionality, including cauldron interactions and block placement logic, while updating the plugin.yml and config.yml to reflect these changes.

--- src/main/java/me/djtmk/InfiniteBuckets/commands/Commands.java
+++ src/main/java/me/djtmk/InfiniteBuckets/commands/Commands.java
@@ -1,14 +1,12 @@
 package me.djtmk.InfiniteBuckets.commands;
 
 import me.djtmk.InfiniteBuckets.Main;
-import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import java.text.DecimalFormat;
 
 public class Commands implements CommandExecutor {
 
@@ -20,69 +18,98 @@ public class Commands implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "Only players can use that command!");
+        if (!(sender instanceof Player) && args.length > 0 && !args[0].equalsIgnoreCase("reload")) {
+            sender.sendMessage(ChatColor.RED + "Only players can use most inf commands!");
             return true;
         }
 
-        Player player = (Player) sender;
-        Economy economy = plugin.getEconomy();
-        DecimalFormat formatter = new DecimalFormat("#,###");
+        Player player = (sender instanceof Player) ? (Player) sender : null;
 
-        switch (cmd.getName().toLowerCase()) {
-            case "infwater":
-                handleInfiniteBucketPurchase(player, args, "water", plugin.getConfig().getInt("water.cost"), economy, formatter, plugin.getItemManager().infiniteWaterBucket());
-                break;
+        if (args.length == 0) {
+            sendHelp(sender);
+            return true;
+        }
 
-            case "inflava":
-                handleInfiniteBucketPurchase(player, args, "lava", plugin.getConfig().getInt("lava.cost"), economy, formatter, plugin.getItemManager().infiniteLavaBucket());
-                break;
-
-            case "infinitebuckets":
-                if (player.hasPermission("infbuckets.admin")) {
-                    handleAdminCommand(player, args);
+        switch (args[0].toLowerCase()) {
+            case "water":
+                if (player == null) {
+                    sender.sendMessage(ChatColor.RED + "This command is for players only!");
+                    return true;
+                }
+                if (player.hasPermission("inf.use.water")) {
+                    player.getInventory().addItem(plugin.getItemManager().infiniteWaterBucket());
+                    player.sendMessage(ChatColor.GREEN + "You received an Infinite Water Bucket!");
                 } else {
-                    player.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                    player.sendMessage(ChatColor.RED + "You don't have permission to obtain an infinite water bucket!");
+                }
+                break;
+
+            case "lava":
+                if (player == null) {
+                    sender.sendMessage(ChatColor.RED + "This command is for players only!");
+                    return true;
+                }
+                if (player.hasPermission("inf.use.lava")) {
+                    player.getInventory().addItem(plugin.getItemManager().infiniteLavaBucket());
+                    player.sendMessage(ChatColor.GREEN + "You received an Infinite Lava Bucket!");
+                } else {
+                    player.sendMessage(ChatColor.RED + "You don't have permission to obtain an infinite lava bucket!");
+                }
+                break;
+
+            case "reload":
+                if (sender.hasPermission("inf.admin")) {
+                    plugin.reloadConfig();
+                    sender.sendMessage(ChatColor.GREEN + "Configuration reloaded.");
+                } else {
+                    sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                }
+                break;
+
+            case "give":
+                if (!sender.hasPermission("inf.admin")) {
+                    sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                    return true;
+                }
+                if (args.length != 3) {
+                    sender.sendMessage(ChatColor.RED + "Usage: /inf give <player> <water|lava>");
+                    return true;
+                }
+                Player target = Bukkit.getPlayer(args[1]);
+                if (target == null) {
+                    sender.sendMessage(ChatColor.RED + "Player " + args[1] + " not found!");
+                    return true;
+                }
+                switch (args[2].toLowerCase()) {
+                    case "water":
+                        target.getInventory().addItem(plugin.getItemManager().infiniteWaterBucket());
+                        target.sendMessage(ChatColor.GREEN + "You received an Infinite Water Bucket!");
+                        sender.sendMessage(ChatColor.GREEN + "Gave " + target.getName() + " an Infinite Water Bucket!");
+                        break;
+                    case "lava":
+                        target.getInventory().addItem(plugin.getItemManager().infiniteLavaBucket());
+                        target.sendMessage(ChatColor.GREEN + "You received an Infinite Lava Bucket!");
+                        sender.sendMessage(ChatColor.GREEN + "Gave " + target.getName() + " an Infinite Lava Bucket!");
+                        break;
+                    default:
+                        sender.sendMessage(ChatColor.RED + "Invalid bucket type! Use 'water' or 'lava'");
                 }
                 break;
 
             default:
-                player.sendMessage(ChatColor.RED + "Unknown command!");
+                sendHelp(sender);
         }
 
         return true;
     }
 
-    private void handleInfiniteBucketPurchase(Player player, String[] args, String type, int cost, Economy economy, DecimalFormat formatter, Object bucket) {
-        if (!player.hasPermission("infbuckets.obtain")) {
-            player.sendMessage(ChatColor.RED + "You do not have permission to buy an infinite " + type + " bucket!");
-            return;
-        }
-
-        if (args.length == 0) {
-            player.sendMessage("");
-            player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "You are about to purchase an infinite " + type + " bucket");
-            player.sendMessage(ChatColor.GREEN + "This costs " + formatter.format(cost) + ". Type /inf" + type + " confirm to buy it.");
-            player.sendMessage("");
-        } else if (args.length == 1 && args[0].equalsIgnoreCase("confirm")) {
-            if (economy.getBalance(player) >= cost) {
-                economy.withdrawPlayer(player, cost);
-                player.getInventory().addItem((org.bukkit.inventory.ItemStack) bucket);
-                player.sendMessage(ChatColor.GREEN + "You have bought an Infinite " + type + " bucket!");
-            } else {
-                player.sendMessage(ChatColor.RED + "You do not have enough money!");
-            }
-        } else {
-            player.sendMessage(ChatColor.RED + "Usage: /inf" + type + " [confirm]");
-        }
-    }
-
-    private void handleAdminCommand(Player player, String[] args) {
-        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-            plugin.reloadConfig();
-            player.sendMessage(ChatColor.GREEN + "Configuration reloaded.");
-        } else {
-            player.sendMessage(ChatColor.RED + "Usage: /infinitebuckets reload");
+    private void sendHelp(CommandSender sender) {
+        sender.sendMessage(ChatColor.GOLD + "Infinite Buckets Commands:");
+        sender.sendMessage(ChatColor.YELLOW + "/inf water" + ChatColor.WHITE + " - Get an infinite water bucket");
+        sender.sendMessage(ChatColor.YELLOW + "/inf lava" + ChatColor.WHITE + " - Get an infinite lava bucket");
+        if (sender.hasPermission("inf.admin")) {
+            sender.sendMessage(ChatColor.YELLOW + "/inf reload" + ChatColor.WHITE + " - Reload configuration");
+            sender.sendMessage(ChatColor.YELLOW + "/inf give <player> <water|lava>" + ChatColor.WHITE + " - Give a bucket to a player");
         }
     }
 }
--- src/main/java/me/djtmk/InfiniteBuckets/item/ItemEvents.java
+++ src/main/java/me/djtmk/InfiniteBuckets/item/ItemEvents.java
@@ -3,6 +3,9 @@ package me.djtmk.InfiniteBuckets.item;
 import me.djtmk.InfiniteBuckets.Main;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Levelled;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,32 +25,49 @@ public class ItemEvents implements Listener {
     @EventHandler
     public void onBucketDrain(PlayerBucketEmptyEvent event) {
         Player player = event.getPlayer();
-
-        int x = event.getBlock().getX();
-        int y = event.getBlock().getY();
-        int z = event.getBlock().getZ();
-
         ItemStack bucket = player.getInventory().getItemInMainHand();
 
-        // no meta
+        // Check if the item has metadata
         if (!bucket.hasItemMeta()) return;
 
         NamespacedKey infinite = new NamespacedKey(plugin, "infinite");
         PersistentDataContainer container = bucket.getItemMeta().getPersistentDataContainer();
 
-        // not an infinite bucket
+        // Check if it's an infinite bucket
         if (!container.has(infinite, PersistentDataType.INTEGER)) return;
 
-        // infinite water
-        if (container.get(infinite, PersistentDataType.INTEGER) == 0) {
-            player.getWorld().getBlockAt(x, y, z).setType(Material.WATER);
-            event.setCancelled(true);
+        Block clickedBlock = event.getBlockClicked();
+        BlockFace face = event.getBlockFace();
+        Block targetBlock = clickedBlock.getRelative(face);
+
+        Integer bucketType = container.get(infinite, PersistentDataType.INTEGER);
+
+        // Handle cauldron filling
+        if (clickedBlock.getType() == Material.CAULDRON) {
+            if (bucketType == 0) { // Water
+                clickedBlock.setType(Material.WATER_CAULDRON);
+                Levelled cauldronData = (Levelled) clickedBlock.getBlockData();
+                cauldronData.setLevel(cauldronData.getMaximumLevel());
+                clickedBlock.setBlockData(cauldronData);
+                event.setCancelled(true);
+            } else if (bucketType == 1) { // Lava
+                clickedBlock.setType(Material.LAVA_CAULDRON);
+                event.setCancelled(true);
+            }
+            return;
         }
 
-        // infinite lava
-        if (container.get(infinite, PersistentDataType.INTEGER) == 1) {
-            player.getWorld().getBlockAt(x, y, z).setType(Material.LAVA);
-            event.setCancelled(true);
+        // Place liquid in the adjacent block
+        if (bucketType == 0) { // Infinite water
+            if (targetBlock.getType().isAir() || targetBlock.getType() == Material.WATER) {
+                targetBlock.setType(Material.WATER);
+                event.setCancelled(true);
+            }
+        } else if (bucketType == 1) { // Infinite lava
+            if (targetBlock.getType().isAir() || targetBlock.getType() == Material.LAVA) {
+                targetBlock.setType(Material.LAVA);
+                event.setCancelled(true);
+            }
         }
     }
 }

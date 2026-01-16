ae263727c1ceb95ca82eb824a5c88e3144d4a33e
djtmk-admin <troymkrause@gmail.com>
2026-01-15 21:44:54 -0500

    chore: remove unused classes, configurations, and resources related to InfiniteBuckets functionality

--- src/main/java/me/djtmk/InfiniteBuckets/hooks/protectionhook/ProtectionHook.java
+++
@@ -1,8 +0,0 @@
-package me.djtmk.InfiniteBuckets.hooks.protectionhook;
-
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-
-public interface ProtectionHook {
-    boolean canBuild(Player player, Block block);
-}
